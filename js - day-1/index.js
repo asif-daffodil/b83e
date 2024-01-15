f57b031c6dbd27e7b8580b5696b2e41d159ab9a9
asif-daffodil <abir@dipti.com.bd>
2024-01-15 19:22:00 +0600

    jan-15-2024

---
+++ js - day-1/index.js
@@ -0,0 +1,44 @@
+
+var capital_123 = "Dhaka";
+console.log(capital_123);
+
+// this is a comment
+/*
+    this is a 
+    multiline 
+    comment
+*/
+
+// Data Types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Object
+
+console.log(typeof kakku);
+console.log(typeof 123);
+console.log(typeof "kakku");
+console.log(typeof true);
+console.log(typeof null);
+
+// Operators
+// Arithmetic Operators: +, -, *, /, %, ++, --
+
+// Assignment Operators: =, +=, -=, *=, /=, %=
+
+var x = 10;
+x += 5;
+console.log(x);
+
+// Comparison Operators: ==, ===, !=, !==, >, <, >=, <=
+
+var a = 10;
+var b = "10";
+
+console.log(a !== b);
+
+// Logical Operators: &&, ||, !
+
+
+
