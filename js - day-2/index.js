a7641c5bde1327f040965a8b38d519be52900a7d
asif-daffodil <abir@dipti.com.bd>
2024-01-18 19:35:20 +0600

    jan-18-2024

---
+++ js - day-2/index.js
@@ -0,0 +1,13 @@
+function asif(sazal = 420, parvez = 69) {
+    return sazal + parvez;
+}
+
+console.log(asif(5, 3));
+console.log(asif(3, 9));
+
+var x = 5;
+var y = 7;
+console.log(asif(x, y));
+
+console.log(asif());
+console.log(asif(20));
